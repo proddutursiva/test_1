c76265a58dae7d3f775ecc80b1fe256c44be2bca
proddutursiva123 <proddutursiva123@gmail.com>
2024-11-08 10:03:43 +0530

    commiting from intaliji idea

---
+++ src/TestBean.java
@@ -0,0 +1,34 @@
+public class TestBean {
+
+    private int id;
+    private String name;
+
+    public TestBean(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TestBean{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
